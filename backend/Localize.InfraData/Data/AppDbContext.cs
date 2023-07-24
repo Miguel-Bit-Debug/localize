f7faca86e49db77378c4df3ceae1f7c50a9d2d22
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-07-24 16:32:27 -0300

    changes

---
+++ backend/Localize.InfraData/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Localize.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Localize.InfraData.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Revenue> Pedidos { get; set; }
+        public DbSet<Account> Contas { get; set; }
+    }
+}
